520f5b563c907cb5baab9635472cdc18c19b5280
mike <mike@mikes-Air.lan>
2024-08-06 14:30:55 -0500

    some ui updates

--- app/records/[id]/page.tsx
+++ app/records/[id]/page.tsx
@@ -4,13 +4,22 @@ const Record = async ({ params }) => {
   const { id } = params;
   const record = await fetchRecord(id)
   return (
-    <div>
+    <div className="max-w-2xl mx-auto">
+      <div className="flex justify-between mb-4">
+        <button className="primary-button px-3">Back</button>
+        <button className="primary-button px-3">Edit</button>
+      </div>
       <p>
         {record.name}
       </p>
       <p>
         {record.artist_name}
       </p>
+      {record?.album_art_url &&
+        <p>
+          {record.album_art_url}
+        </p>
+      }
     </div>
   )
 };
