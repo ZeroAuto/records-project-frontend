a43e7f40c0e1e03c1438b23e6bdb3b14cd0af1c4
mike <mike@mikes-Air.lan>
2024-08-06 14:48:17 -0500

    update single record page

--- app/records/[id]/page.tsx
+++ app/records/[id]/page.tsx
@@ -1,24 +1,64 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import LoadingSpinner from '../../../components/LoadingSpinner';
 import { fetchRecord } from '../../../utils/server.ts';
 
-const Record = async ({ params }) => {
+const Record = ({ params }) => {
   const { id } = params;
-  const record = await fetchRecord(id)
+  const router = useRouter()
+  const [ record, setRecord ] = useState({});
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState('');
+  
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetchRecord(id);
+      setRecord(res);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleBack = () => {
+    router.push('/');
+  }
+
   return (
     <div className="max-w-2xl mx-auto">
       <div className="flex justify-between mb-4">
-        <button className="primary-button px-3">Back</button>
+        <button
+          className="primary-button px-3"
+          onClick={handleBack}
+        >Back</button>
         <button className="primary-button px-3">Edit</button>
       </div>
-      <p>
-        {record.name}
-      </p>
-      <p>
-        {record.artist_name}
-      </p>
-      {record?.album_art_url &&
-        <p>
-          {record.album_art_url}
-        </p>
+      {loading &&
+        <LoadingSpinner />
+      }
+      {!loading && record &&
+        <div>
+          <p>
+            {record.name}
+          </p>
+          <p>
+            {record.artist_name}
+          </p>
+          {record?.album_art_url &&
+            <p>
+              {record.album_art_url}
+            </p>
+          }
+        </div>
       }
     </div>
   )
